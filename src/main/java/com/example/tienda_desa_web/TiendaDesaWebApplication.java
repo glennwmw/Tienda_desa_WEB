49a966a2391772facfe0713d0a2d3f641359f1a6
Glennmar Milliner <gmilliner@vmware.com>
2022-02-02 09:13:53 -0600

    Initial commit

---
+++ src/main/java/com/example/tienda_desa_web/TiendaDesaWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.tienda_desa_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDesaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaDesaWebApplication.class, args);
+    }
+
+}
